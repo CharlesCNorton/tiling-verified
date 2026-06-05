12776021c136eba20c1173b9515f705f60c4d356
CharlesCNorton <machineelv@gmail.com>
2026-06-05 12:57:53 -0400

    Integrate lambda-box realizer extraction into Tiling.v
    Folds LambdaBox.v into the proof-term/realizer section: the lambda_box calculus, size-decreasing beta_box_step with normalises_exists, the has_type system, and extract_realizer with its typing and normalisation. Reuses Provable_term. Removes LambdaBox.v.

--- Tiling.v
+++ Tiling.v
@@ -17659,6 +17659,281 @@ Proof.
   - intros phi pt. apply le_n.
 Qed.
 
+(** ** Curry-Howard realizer extraction: the lambda-box calculus.
+
+    [lambda_box] is a typed term calculus with combinator constants for
+    the axioms, application/abstraction, pairing, graded box intro/elim,
+    and a Loeb-fixpoint node carrying the API [loeb_fixpoint].
+    [beta_box_step] is size-decreasing ([step_size]), so every term
+    normalises ([normalises_exists]).  [extract_realizer] maps a
+    [Provable_term] to a realizer of the same type
+    ([extract_realizer_typed]) with a normal form
+    ([extract_realizer_reduces]). *)
+
+Inductive lambda_box : Type :=
+  | tVar      : nat -> lambda_box
+  | tK        : Form -> Form -> lambda_box
+  | tS        : Form -> Form -> Form -> lambda_box
+  | tDN       : Form -> lambda_box
+  | tBoxK     : nat -> Form -> Form -> lambda_box
+  | tLoeb     : nat -> Form -> lambda_box
+  | tBox4     : nat -> Form -> lambda_box
+  | tMon      : nat -> Form -> lambda_box
+  | tNextCon  : nat -> lambda_box
+  | tApp      : lambda_box -> lambda_box -> lambda_box
+  | tAbs      : Form -> lambda_box -> lambda_box
+  | tPair     : lambda_box -> lambda_box -> lambda_box
+  | tFst      : lambda_box -> lambda_box
+  | tSnd      : lambda_box -> lambda_box
+  | tBoxI     : nat -> lambda_box -> lambda_box
+  | tBoxE     : lambda_box -> lambda_box
+  | tLoebFix  : nat -> Form -> lambda_box -> lambda_box.
+
+(** The higher-order Loeb fixpoint API; the inductive stays strictly
+    positive since [f] is applied to a fresh variable rather than
+    stored. *)
+
+Definition loeb_fixpoint (n : nat) (phi : Form)
+  (f : lambda_box -> lambda_box) : lambda_box :=
+  tLoebFix n phi (f (tVar 0)).
+
+Fixpoint tsize (t : lambda_box) : nat :=
+  match t with
+  | tVar _ => 1
+  | tK _ _ => 1
+  | tS _ _ _ => 1
+  | tDN _ => 1
+  | tBoxK _ _ _ => 1
+  | tLoeb _ _ => 1
+  | tBox4 _ _ => 1
+  | tMon _ _ => 1
+  | tNextCon _ => 1
+  | tApp f x => 1 + tsize f + tsize x
+  | tAbs _ b => 1 + tsize b
+  | tPair x y => 1 + tsize x + tsize y
+  | tFst x => 1 + tsize x
+  | tSnd x => 1 + tsize x
+  | tBoxI _ x => 1 + tsize x
+  | tBoxE x => 1 + tsize x
+  | tLoebFix _ _ b => 1 + tsize b
+  end.
+
+(** One-step beta-box reduction: redexes + full congruence. *)
+
+Inductive beta_box_step : lambda_box -> lambda_box -> Prop :=
+  | st_K    : forall a b m n,
+      beta_box_step (tApp (tApp (tK a b) m) n) m
+  | st_Fst  : forall x y, beta_box_step (tFst (tPair x y)) x
+  | st_Snd  : forall x y, beta_box_step (tSnd (tPair x y)) y
+  | st_BoxE : forall k x, beta_box_step (tBoxE (tBoxI k x)) x
+  | st_App1 : forall f f' x,
+      beta_box_step f f' -> beta_box_step (tApp f x) (tApp f' x)
+  | st_App2 : forall f x x',
+      beta_box_step x x' -> beta_box_step (tApp f x) (tApp f x')
+  | st_Abs  : forall a b b',
+      beta_box_step b b' -> beta_box_step (tAbs a b) (tAbs a b')
+  | st_Pair1 : forall x x' y,
+      beta_box_step x x' -> beta_box_step (tPair x y) (tPair x' y)
+  | st_Pair2 : forall x y y',
+      beta_box_step y y' -> beta_box_step (tPair x y) (tPair x y')
+  | st_Fst1 : forall x x',
+      beta_box_step x x' -> beta_box_step (tFst x) (tFst x')
+  | st_Snd1 : forall x x',
+      beta_box_step x x' -> beta_box_step (tSnd x) (tSnd x')
+  | st_BoxI1 : forall k x x',
+      beta_box_step x x' -> beta_box_step (tBoxI k x) (tBoxI k x')
+  | st_BoxE1 : forall x x',
+      beta_box_step x x' -> beta_box_step (tBoxE x) (tBoxE x')
+  | st_LoebFix1 : forall k a b b',
+      beta_box_step b b' -> beta_box_step (tLoebFix k a b) (tLoebFix k a b').
+
+Theorem beta_box_step_nontrivial :
+  exists t t', beta_box_step t t'.
+Proof.
+  exists (tApp (tApp (tK Bot Bot) (tVar 0)) (tVar 1)), (tVar 0).
+  apply st_K.
+Qed.
+
+Theorem step_size : forall t t',
+  beta_box_step t t' -> tsize t' < tsize t.
+Proof.
+  intros t t' H. induction H; cbn in *; lia.
+Qed.
+
+Definition Normal (t : lambda_box) : Prop :=
+  forall t', ~ beta_box_step t t'.
+
+Inductive star : lambda_box -> lambda_box -> Prop :=
+  | star_refl : forall t, star t t
+  | star_step : forall t1 t2 t3,
+      beta_box_step t1 t2 -> star t2 t3 -> star t1 t3.
+
+Definition beta_box_normalises (t nf : lambda_box) : Prop :=
+  star t nf /\ Normal nf.
+
+Lemma not_normal_has_step : forall t,
+  ~ Normal t -> exists t', beta_box_step t t'.
+Proof.
+  intros t H. apply NNPP. intro Hno.
+  apply H. intros t' Hs. apply Hno. exists t'. exact Hs.
+Qed.
+
+Lemma normalises_bounded : forall k t,
+  tsize t <= k -> exists nf, beta_box_normalises t nf.
+Proof.
+  induction k as [|k IH]; intros t Hk.
+  - destruct t; cbn in Hk; lia.
+  - destruct (classic (Normal t)) as [HN | HN].
+    + exists t. split; [apply star_refl | exact HN].
+    + destruct (not_normal_has_step t HN) as [t' Hstep].
+      pose proof (step_size _ _ Hstep) as Hlt.
+      assert (Hk' : tsize t' <= k) by lia.
+      destruct (IH t' Hk') as [nf [Hstar HNnf]].
+      exists nf. split; [eapply star_step; [exact Hstep | exact Hstar] | exact HNnf].
+Qed.
+
+Theorem normalises_exists : forall t,
+  exists nf, beta_box_normalises t nf.
+Proof.
+  intro t. apply (normalises_bounded (tsize t) t). lia.
+Qed.
+
+Inductive has_type : list Form -> lambda_box -> Form -> Prop :=
+  | ht_var : forall G i A,
+      nth_error G i = Some A -> has_type G (tVar i) A
+  | ht_K : forall G a b,
+      has_type G (tK a b) (Impl a (Impl b a))
+  | ht_S : forall G a b c,
+      has_type G (tS a b c)
+        (Impl (Impl a (Impl b c)) (Impl (Impl a b) (Impl a c)))
+  | ht_DN : forall G a,
+      has_type G (tDN a) (Impl (Neg (Neg a)) a)
+  | ht_BoxK : forall G n a b,
+      has_type G (tBoxK n a b)
+        (Impl (Box n (Impl a b)) (Impl (Box n a) (Box n b)))
+  | ht_Loeb : forall G n a,
+      has_type G (tLoeb n a)
+        (Impl (Box n (Impl (Box n a) a)) (Box n a))
+  | ht_Box4 : forall G n a,
+      has_type G (tBox4 n a) (Impl (Box n a) (Box n (Box n a)))
+  | ht_Mon : forall G n a,
+      has_type G (tMon n a) (Impl (Box n a) (Box (S n) a))
+  | ht_NextCon : forall G n,
+      has_type G (tNextCon n) (Box (S n) (Neg (Box n Bot)))
+  | ht_App : forall G f x a b,
+      has_type G f (Impl a b) -> has_type G x a ->
+      has_type G (tApp f x) b
+  | ht_Abs : forall G a b body,
+      has_type (a :: G) body b -> has_type G (tAbs a body) (Impl a b)
+  | ht_Pair : forall G x y a b,
+      has_type G x a -> has_type G y b ->
+      has_type G (tPair x y) (And a b)
+  | ht_Fst : forall G p a b,
+      has_type G p (And a b) -> has_type G (tFst p) a
+  | ht_Snd : forall G p a b,
+      has_type G p (And a b) -> has_type G (tSnd p) b
+  | ht_BoxI : forall G n a x,
+      has_type G x a -> has_type G (tBoxI n x) (Box n a)
+  | ht_BoxE : forall G n a x,
+      has_type G x (Box n a) -> has_type G (tBoxE x) a
+  | ht_LoebFix : forall G n a body,
+      has_type (Box n a :: G) body a ->
+      has_type G (tLoebFix n a body) (Box n a).
+
+Theorem loeb_fixpoint_typed : forall G n phi f,
+  has_type (Box n phi :: G) (f (tVar 0)) phi ->
+  has_type G (loeb_fixpoint n phi f) (Box n phi).
+Proof.
+  intros G n phi f H. unfold loeb_fixpoint. apply ht_LoebFix. exact H.
+Qed.
+
+Fixpoint extract_realizer (phi : Form) (pt : Provable_term phi) : lambda_box :=
+  match pt with
+  | pt_K a b => tK a b
+  | pt_S a b c => tS a b c
+  | pt_DN a => tDN a
+  | pt_BoxK n a b => tBoxK n a b
+  | pt_Loeb n a => tLoeb n a
+  | pt_Box4 n a => tBox4 n a
+  | pt_Mon n a => tMon n a
+  | pt_NextCon n => tNextCon n
+  | pt_MP a b pq p =>
+      tApp (extract_realizer (Impl a b) pq) (extract_realizer a p)
+  | pt_Nec n a p => tBoxI n (extract_realizer a p)
+  end.
+
+Theorem extract_realizer_typed : forall phi (pt : Provable_term phi),
+  has_type [] (extract_realizer phi pt) phi.
+Proof.
+  intros phi pt. induction pt; cbn.
+  - apply ht_K.
+  - apply ht_S.
+  - apply ht_DN.
+  - apply ht_BoxK.
+  - apply ht_Loeb.
+  - apply ht_Box4.
+  - apply ht_Mon.
+  - apply ht_NextCon.
+  - eapply ht_App; [exact IHpt1 | exact IHpt2].
+  - apply ht_BoxI; exact IHpt.
+Qed.
+
+Theorem extract_realizer_reduces : forall phi (pt : Provable_term phi),
+  exists nf, beta_box_normalises (extract_realizer phi pt) nf.
+Proof.
+  intros phi pt. apply normalises_exists.
+Qed.
+
+(** A closed realizer of [Top] (the SKI identity I = S K K). *)
+
+Definition I_term : Provable_term Top :=
+  pt_MP _ _
+    (pt_MP _ _ (pt_S Bot (Impl Bot Bot) Bot) (pt_K Bot (Impl Bot Bot)))
+    (pt_K Bot Bot).
+
+(** A proof term applying the K axiom to two arguments, so its realizer
+    is a K-redex. *)
+
+Definition reducer_term : Provable_term Top :=
+  pt_MP Top Top
+    (pt_MP Top (Impl Top Top) (pt_K Top Top) I_term)
+    I_term.
+
+Theorem extract_reducer_steps :
+  beta_box_step (extract_realizer Top reducer_term)
+                (extract_realizer Top I_term).
+Proof.
+  cbn. apply st_K.
+Qed.
+
+Theorem reducer_term_typed :
+  has_type [] (extract_realizer Top reducer_term) Top.
+Proof.
+  apply extract_realizer_typed.
+Qed.
+
+Theorem lambda_box_realizer_summary :
+  (exists t t', beta_box_step t t') /\
+  (forall t t', beta_box_step t t' -> tsize t' < tsize t) /\
+  (forall phi (pt : Provable_term phi),
+     has_type [] (extract_realizer phi pt) phi) /\
+  (forall phi (pt : Provable_term phi),
+     exists nf, beta_box_normalises (extract_realizer phi pt) nf) /\
+  (beta_box_step (extract_realizer Top reducer_term)
+                 (extract_realizer Top I_term)) /\
+  (forall G n phi f,
+     has_type (Box n phi :: G) (f (tVar 0)) phi ->
+     has_type G (loeb_fixpoint n phi f) (Box n phi)).
+Proof.
+  split; [|split; [|split; [|split; [|split]]]].
+  - exact beta_box_step_nontrivial.
+  - exact step_size.
+  - exact extract_realizer_typed.
+  - exact extract_realizer_reduces.
+  - exact extract_reducer_steps.
+  - exact loeb_fixpoint_typed.
+Qed.
+
 Theorem Critch_polynomial_bound_is_polynomial :
   is_polynomial Critch_polynomial_bound.
 Proof. exists 1, 1, 1. intros x. unfold Critch_polynomial_bound. lia. Qed.
